d46032be9da221b923e0105f27cab418e073fc3c
zoereedviz <zoereed020@gmail.com>
2025-03-11 11:41:47 +0000

    challenge_03 completed

---
+++ challenge_03/models/office_weather/all_weather_readings.sql
@@ -0,0 +1,11 @@
+{{ config(
+    materialized= 'incremental',
+    unique_key= ['office','time']
+) }}
+
+SELECT *
+FROM {{ ref('extract_weather_data') }}
+
+{% if is_incremental() %}
+    WHERE time >= (SELECT MAX(time) FROM {{ this }})
+{% endif %}
